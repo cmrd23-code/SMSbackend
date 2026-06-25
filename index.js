85fd21587bb7e1154c1707fb8bfc9acb0b5a3656
cmrd23-code <cmrd23@gmail.com>
2026-06-25 12:18:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const connectDb = require("./config/connectDb");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
